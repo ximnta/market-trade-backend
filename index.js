76bd51bc002b210757c6f97f33598e5777b666ac
Ximena Tena <ximnta@gmail.com>
2018-06-30 14:09:51 +0100

    Add db

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
 const cool = require('cool-ascii-faces')
 const express = require('express')
 const path = require('path')
@@ -17,8 +22,20 @@ express()
     }
     res.send(result)
   })
+  .get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect()
+      const result = await client.query('SELECT * FROM test_table');
+      res.render('pages/db', result);
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
   
 
-  
+
+
