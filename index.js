3e00e69f3a38cb8f4a8db7b767dd0a52012bbd95
Ximena Tena <ximnta@gmail.com>
2018-07-03 22:08:25 +0100

    Add json schema

--- index.js
+++ index.js
@@ -4,10 +4,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+const jsonschema = require('jsonschema');
+const validator = new jsonschema.Validator();
+const schema = require('./schema.json');
 const app = express();
 let tradeList = [];
 
 app.use(express.static('public'));
+// let validator = require('./validateJson');
 
 app.use(cors());
 
@@ -17,22 +21,34 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // parse application/json
 app.use(bodyParser.json());
 
+app.use((err, req, res, next) => {
+  if (err) {
+    return res.status(err.status).send('Invalid Request data')
+  } else {
+    next()
+  }
+})
+
 // POST
-app.post('/api/trademsg', function(req, res) {
-  
+app.post('/api/trademsg', function (req, res) {
   let t = req.body;
+
+  let errors = validator.validate(t, schema).errors;
+  if (errors.length > 0) {
+    return res.status(400).send(JSON.stringify(errors, null, 2));
+  }
 
   tradeList.push(t);
   res.send(JSON.stringify(t, null, 2));
 });
 
 // GET
-app.get('/api/trademsg', function(req, res) {
+app.get('/api/trademsg', function (req, res) {
   res.send(JSON.stringify(tradeList, null, 2));
 });
 
-// GET
-app.get('/api/reset', function(req, res) {
+// GET: Reset
+app.get('/api/reset', function (req, res) {
   tradeList = [];
   res.send("Data reset");
 });
